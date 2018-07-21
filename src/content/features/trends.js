6d6c749b34f03bc416a0786185ae64dc9417ef9d
Santosh Venkatraman <santosh.venk@gmail.com>
2018-07-21 20:00:27 +0530

    Fixes semi-colon missing in hideTrends()

--- src/content/features/trends.js
+++ src/content/features/trends.js
@@ -2,7 +2,7 @@ import { hideDOMElement } from "./hide-element";
 import { observeElement } from "./observe-element";
 
 export const hideTrends = () => {
-    const selector = ".module.Trends.trends" // Trends
+    const selector = ".module.Trends.trends"; // Trends
     observeElement({
         selector,
         callback: elements => {
